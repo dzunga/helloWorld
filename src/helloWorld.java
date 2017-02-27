9d67156c30e66bf81bd6f41c3784eb35447e51b7
dzunga <dzungle1@yahoo.com>
2017-02-27 17:57:13 +0800

    add helloworld3 line update

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -4,6 +4,7 @@ public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 
 	}
 
